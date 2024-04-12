613079d10eb8f10bcf746f388e5f79bc7bd6e1ea
FerPascualF50 <pascualefernando@gmail.com>
2024-04-12 13:48:40 -0300

    update: env to port

--- api/src/server.js
+++ api/src/server.js
@@ -2,12 +2,13 @@ import express from "express";
 import { userRouter } from "./routes/user/userRoutes.js";
 
 const server = express();
+const PORT = process.env.PORT || 4000;
 
 //Midelwares
-server.use(express.json())
+server.use(express.json());
 
 //Rutas
 server.use("/api/user", userRouter)
 
 
-server.listen(4000, ()=> console.log(`API REST linstening on port ****`))
+server.listen(PORT, ()=> console.log(`API REST linstening on port ****`))
