34da89f940a6aefe86107f2b3bb325f860d89d16
FerPascualF50 <pascualefernando@gmail.com>
2024-05-07 00:53:34 -0300

    update: avatar

--- client/src/components/AvatarMenu.jsx
+++ client/src/components/AvatarMenu.jsx
@@ -1,51 +1,60 @@
-import * as React from 'react';
-import {Box, Tooltip, IconButton, Avatar, Menu, MenuItem, Typography } from '@mui/material'
-import { useSelector } from 'react-redux'
+import { useState } from 'react';
+import { Box, Tooltip, IconButton, Avatar, Menu, MenuItem, Typography } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+
+
 
 const AvatarMenu = () => {
-  const { user } = useSelector((state) => state.auth)
-  const settings = ['Mi perfil', 'Dashboard', 'Logout'];
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const isLoggedIn = localStorage.getItem('access_token');
+  const [anchorElUser, setAnchorElUser] = useState(null);
+  const navigate = useNavigate()
 
+  const handleLogout = () => {
+    localStorage.removeItem('access_token')
+    navigate('/')
+  }
+
+  
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
   };
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   }
-
-  if(user)
-  return (
-    <Box sx={{ flexGrow: 0 }}>
-      <Tooltip title="Open settings">
-        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-          <Avatar  src="/static/images/avatar/2.jpg" alt="Avatar_image" />
-        </IconButton>
-      </Tooltip>
-      <Menu
-        sx={{ mt: '45px' }}
-        id="menu-appbar"
-        anchorEl={anchorElUser}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        keepMounted
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        open={Boolean(anchorElUser)}
-        onClose={handleCloseUserMenu}
-      >
-        {settings.map((setting) => (
-          <MenuItem key={setting} onClick={handleCloseUserMenu}>
-            <Typography textAlign="center">{setting}</Typography>
-          </MenuItem>
-        ))}
-      </Menu>
-    </Box>
-  )
+  const settings = [
+    { name: '     X       ', onClick: () => { } },
+    { name: 'Mi perfil', onClick: () => { } },
+    { name: 'Dashboard', onClick: () => { } },
+    { name: 'Salir', onClick: handleLogout },
+  ];
+  
+  if (isLoggedIn)
+    return (
+      <Box sx={{ flexGrow: 0 }}>
+        <Tooltip title="Open settings">
+          <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+            <Avatar src="/static/images/avatar/2.jpg" alt="Avatar_image" />
+          </IconButton>
+        </Tooltip>
+        <Menu
+          sx={{ mt: '45px' }}
+          id="menu-appbar"
+          anchorEl={anchorElUser}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}
+        >
+          {settings.map((setting) => (
+            <MenuItem key={setting.name} onClick={setting.onClick}>
+              <Typography textAlign="center">{setting.name}</Typography>
+            </MenuItem>
+          ))}
+        </Menu>
+      </Box>
+    )
 }
 
 export default AvatarMenu;
