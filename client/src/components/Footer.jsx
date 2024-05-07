c02ba1c96a8cd23d8fa65558ddf01c5e1ce4be10
FerPascualF50 <pascualefernando@gmail.com>
2024-05-07 18:34:02 -0300

    update: Footer componenet

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,28 +5,22 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link sx={{cursor: 'pointer'}} color="inherit" href="https://www.linkedin.com/in/fernando-pascual-full-stack-developer/" target="_blank">
-        Fernando Pascual
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
 export default function Footer() {
   return (
-    <Box sx={{ position: 'fixed', bottom: 0,width: '100%', backgroundColor: '#f3f3f3'}} >
+    <Box sx={{ position: 'fixed', bottom: 0, width: '100%', backgroundColor: '#f3f3f3', maxHeight: '68px'}} >
       <CssBaseline />
-      <Box component="footer" sx={{ py: 3, px: 2, mt: 'auto', backgroundColor: '#f3f3f3'}}>
-        <Container maxWidth="sm">
-          <Copyright />
-        </Container>
-      </Box>
+      <Container maxWidth="sm">
+        <Box component="footer" sx={{ py: 3, px: 2 }}>
+          <Typography variant="body2" color="text.secondary" align="center">
+            {'Copyright © '}
+            <Link sx={{ cursor: 'pointer' }} color="inherit" href="https://www.linkedin.com/in/fernando-pascual-full-stack-developer/" target="_blank">
+              Fernando Pascual
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+          </Typography>
+        </Box>
+      </Container>
     </Box>
   );
 }
