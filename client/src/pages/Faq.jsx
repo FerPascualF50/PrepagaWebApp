24d9e049e0a298d81724184995d1566e171e6708
FerPascualF50 <pascualefernando@gmail.com>
2024-05-07 18:58:03 -0300

    update: Faq component

--- client/src/pages/Faq.jsx
+++ client/src/pages/Faq.jsx
@@ -1,14 +1,8 @@
-import * as React from 'react';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
+import { Accordion, AccordionSummary, AccordionDetails, Grid } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import Grid from '@mui/material/Grid';
-import styles from '../components/styles/faq.module.css'
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../theme';
-
-
+import styles from '../components/styles/faq.module.css'
 
 const Faq = () => {
   const faqs = [
@@ -22,8 +16,6 @@ const Faq = () => {
       title: 'Acceso al servicio', subFaq: [
         { titleSubFaq: '¿Cuándo puedo utilizar los servicios?', answer: 'Al momento de contar con nuestra cobertura, te llegará una credencial que te habilitará como Asociado y te permitirá el acceso a los distintos servicios. Además, podes también generar tu credencial virtual desde esta página web, desde la sección Asociados.' },
         { titleSubFaq: '¿Cómo accedo a la atención?', answer: 'Para recibir la atención se deberá cumplir con las siguientes condiciones: A- Presentar el DNI y la credencial que te acredita como asociado. B- Concurrir a los prestadores que integran la cartilla de acuerdo al plan contratado. C- Tener la cuota abonada al día.' },
-
-
       ]
     },
     {
@@ -31,8 +23,6 @@ const Faq = () => {
         { titleSubFaq: '¿Puedo realizar trámites on line?', answer: 'Si, es muy simple, podés realizar diversas gestiones sin moverte de tu casa y obtener respuesta en forma inmediata. Ingresa a "Mi Cuenta"  y Registrate.' },
         { titleSubFaq: '¿Qué trámites puedo realizar de forma on line?', answer: '- Actualización de datos personales - Gestionar reintegros, autorizaciones, ingreso a programas especiales, cambio de forma de pago, solicitud de credenciales y visualización de facturas.' },
         { titleSubFaq: '¿Cómo genero mi usuario y contraseña para acceder al sitio de Autogestión de Asociados?', answer: '1- Accede al Boton Mi Cuenta y una vez en la página del login, tenes un boton en la parte inferior donde dice hace click si no tenes tu centta, luego  proporcionas tus datos, y eso es todo.' },
-
-
       ]
     },
     {
@@ -45,7 +35,6 @@ const Faq = () => {
       title: 'Autorizaciones', subFaq: [
         { titleSubFaq: '¿Qué requisitos debe cumplimentar un pedido médico?', answer: 'Tu pedido médico debe contener la siguiente información, legible y sin enmiendas ni tachaduras. Recordá que, si faltan datos, no se podrá dar curso a la autorización correspondiente. • Fecha de prescripción • Nombre y apellido • Número de asociado y plan • Diagnóstico claro • Práctica asistencial a realizar, o medicamentos solicitados (nombres genéricos) • Cantidad de prácticas, sesiones, medicamentos, etc. Si es internación, cantidad de días • Firma y sello del profesional prescriptor (con número de matrícula) Los datos deberán ser legibles, sin enmiendas ni tachaduras. Si tu pedido médico es para una práctica, tiene una validez de 60 días desde la fecha de prescripción. Si es para medicamentos, 30 días.' },
         { titleSubFaq: '¿Cómo solicito una autorización?', answer: 'Ens nuestro sitio web ingresando a Autorizaciones . Ahí adjuntá el pedido médico a través de un escaneo o foto legible.' },
-
       ]
     },
   ]
@@ -60,35 +49,28 @@ const Faq = () => {
         </div>
       </div>
       <div className={styles.faq_info}>
-        <Grid container alignItems="center" justifyContent="center">
+        <Grid container alignItems="center" justifyContent="center" paddingBottom={10}>
           <Grid item xs={12} sm={10} md={8} lg={6}>
             {faqs.map((faq, index) => (
               <Accordion key={index} sx={{ '&.Mui-expanded': { border: `1px solid ${theme.palette.primary.main}` } }} >
                 <AccordionSummary
-                  sx={{ color: theme.palette.primary.main, fontWeight: 'bold' }}
+                  sx={{ color: theme.palette.secondary.main, fontWeight: 'bold' }}
                   expandIcon={<ExpandMoreIcon sx={{ color: theme.palette.primary.main }} />}
                   aria-controls={`panel${index + 1}-content`}
-                  id={`panel${index + 1}-header`}
-                >
+                  id={`panel${index + 1}-header`}>
                   {faq.title}
                 </AccordionSummary>
                 <AccordionDetails>
                   {faq.subFaq.map((subFaq, subIndex) => (
                     <Accordion key={subIndex} sx={{ '&.Mui-expanded': { border: `1px solid ${theme.palette.primary.main}` } }} >
                       <AccordionSummary
-                        sx={{ color: theme.palette.secondary.main, fontWeight: 'bold' }}
+                        sx={{ color: theme.palette.primary.main, fontWeight: 'bold' }}
                         expandIcon={<ExpandMoreIcon sx={{ color: theme.palette.secondary.main }} />}
                         aria-controls={`panel${index + 1}-${subIndex + 1}-content`}
-                        id={`panel${index + 1}-${subIndex + 1}-header`}
-                      >
+                        id={`panel${index + 1}-${subIndex + 1}-header`}>
                         {subFaq.titleSubFaq}
                       </AccordionSummary>
-                      <AccordionDetails
-                        sx={{ color: '#807878' }}
-
-                      >
-                        {subFaq.answer}
-                      </AccordionDetails>
+                      <AccordionDetails sx={{ color: '#807878' }}> {subFaq.answer} </AccordionDetails>
                     </Accordion>
                   ))}
                 </AccordionDetails>
