91e82238c47a6011749d4814e3289c0bea0bdebe
FerPascualF50 <pascualefernando@gmail.com>
2024-05-11 19:05:04 -0300

    update: last version Invoicing component

--- client/src/pages/Invoicing.jsx
+++ client/src/pages/Invoicing.jsx
@@ -1,69 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import InvoiceIcon from '@mui/icons-material/Receipt';
-import { Typography, TableContainer, Table, TableHead, TableBody, TableRow, TableCell, Button, Box, } from '@mui/material';
+import { Typography, TableContainer, Table, TableHead, TableBody, TableRow, TableCell, Button, Box } from '@mui/material';
+import toast, { Toaster } from 'react-hot-toast';
 import Loading from '../components/Loading.jsx';
 import { useDispatch, useSelector } from 'react-redux';
-import { useState } from 'react'
-import { getClientsByInvocePeriod } from '../store/invoiceSlice';
+import { getClientsByInvocePeriod, postInvoices } from '../store/invoiceSlice';
 
 const Invoices = () => {
-  // console.log('DATA COMPONENT',data)
   const dispatch = useDispatch();
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
+  const { year, months } = useSelector((state) => state.invoice);
 
-  const handleInvoiceCreation = () => {
-    // Lógica para crear la factura con los datos obtenidos de 'clients'
-  };
-
-  const mockData = {
-    year: 2024,
-    month: [12, 11, 10, 9, 8, 7, 6, 5]
-  };
-
-  const {users} = useSelector((state) => state.invoice);
-  const handleInvoiceClick = async (year, month) => {
-    const period = { year, month };
-    console.log('period',period)
+  const handleGetClientsAndIds = async (year, month) => {
     setLoading(true);
-    try {
-      const response = await dispatch(getClientsByInvocePeriod(period));
-      console.log('Response:', response); // Verifica la respuesta aquí
-      setLoading(false);
-    } catch (error) {
-      console.error('Error:', error);
-      setLoading(false);
-    }
+    const period = { yearInvoice: year, monthInvoice: month };
+    const actionResult = await dispatch(getClientsByInvocePeriod(period));
+    if (!actionResult.payload.success) return toast.error('No existen clientes pendientes de facturar en el período seleccionado');
+    const ids = actionResult.payload.response.map(client => client._id);
+    setLoading(false);
+    handleCreateInvoice(year, month, ids);
+  };
+
+  const handleCreateInvoice = async (year, month, ids) => {
+    setLoading(true);
+    const payload = { year, month, ids };
+    const actionResult = await dispatch(postInvoices(payload));
+    setLoading(false);
+    if (!actionResult.payload.success) toast.error('No había clientes pendientes de facturar')
+    return toast.success('Facturas creadas con exito')
   };
 
   return (
     <Box px={3} py={1}>
       {loading && (<Loading />)}
+      <Toaster position="bottom-center" reverseOrder={false} />
       <Typography variant="h6" sx={{ textAlign: 'center', mb: 2, paddingTop: '10px' }}>
         Periodos disponibles a facturar
       </Typography>
       <TableContainer>
-        <Box >
-          <Table sx={{ minWidth: '100%', maxWidth: '100%', mx: "auto", textAlign: "center" }} >
+        <Box>
+          <Table sx={{ minWidth: '100%', maxWidth: '100%', mx: "auto", textAlign: "center" }}>
             <TableHead sx={{ fontWeight: 'bold' }}>
-              <TableRow >
+              <TableRow>
                 <TableCell sx={{ fontWeight: 'bold' }}>Año</TableCell>
                 <TableCell sx={{ fontWeight: 'bold' }}>Mes</TableCell>
                 <TableCell sx={{ textAlign: 'center', mb: 2, paddingTop: '10px', fontWeight: 'bold' }}>Acción</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {mockData.month.map((month) => (
-                <TableRow key={`${mockData.year}-${month}`}>
-                  <TableCell>{mockData.year}</TableCell>
+              {months?.map((month) => (
+                <TableRow key={`${year}-${month}`}>
+                  <TableCell>{year}</TableCell>
                   <TableCell>{month}</TableCell>
                   <TableCell sx={{ textAlign: 'center', mb: 2, paddingTop: '10px' }}>
-                    <Button
-                      variant="outlined"
-                      color="secondary"
-                      startIcon={<InvoiceIcon />}
-                      onClick={() => handleInvoiceClick(mockData.year, month)}
-                    >
-                      Facturar
+                    <Button variant="outlined" color="secondary" startIcon={<InvoiceIcon />} onClick={() => handleGetClientsAndIds(year, month)} > Facturar
                     </Button>
                   </TableCell>
                 </TableRow>
--- client/src/store/invoiceSlice.js
+++ client/src/store/invoiceSlice.js
@@ -7,8 +7,7 @@ export const getClientsByInvocePeriod = createAsyncThunk(
     try {
       const access_token = localStorage.getItem('access_token');
       const { data } = await axios.get(
-        `/invoices/clients`,
-         period
+        `/invoices/clients?yearInvoice=${period.yearInvoice}&monthInvoice=${period.monthInvoice}`, { headers: { Authorization: access_token } }, {}
       )
       return data
     } catch (error) {
@@ -17,24 +16,58 @@ export const getClientsByInvocePeriod = createAsyncThunk(
   }
 )
 
+export const postInvoices = createAsyncThunk(
+  'invoice/postInvoices',
+  async (payload) => {
+    try {
+      const access_token = localStorage.getItem('access_token');
+      const response = await axios.post(
+        '/invoices',
+        {
+          year: payload.year,
+          month: payload.month,
+          ids: payload.ids
+        },
+        {
+          headers: {
+            Authorization: access_token
+          }
+        }
+      );
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+);
+
 const invoiceSlice = createSlice({
   name: 'invoice',
   initialState: {
-    users: null
+    users: null,
+    year: 2024,
+    months: [5, 6, 7, 8, 9, 10, 11, 12]
   },
   reducers: {
-
-    getClients: (state, action) => {
-      state.users = action.payload
+    setYearAndMonths: (state, action) => {
+      state.year = action.payload.year;
+      state.months = action.payload.months;
+    },
+    removeMonth: (state, action) => {
+      state.months = state.months.filter(item => item !== action.payload);
     }
   },
   extraReducers: (builder) => {
     builder.addCase(getClientsByInvocePeriod.fulfilled, (state, action) => {
-      state.users = action.payload || null
-    })
-
+      state.users = action.payload || null;
+    }),
+      builder.addCase(postInvoices.fulfilled, (state, action) => {
+        state.users = action.payload || null;
+        state.months = state.months.filter(month => month !== action.payload.response[0].period.month);
+      })
   }
-})
+});
 
-export const { getClients } = invoiceSlice.actions
+export const { setYearAndMonths, removeMonth } = invoiceSlice.actions
 export default invoiceSlice.reducer
