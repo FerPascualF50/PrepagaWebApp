125d06ce9f168a79ac5792617b8aa5330db345b6
FerPascualF50 <pascualefernando@gmail.com>
2024-05-12 03:06:13 -0300

    feat: create resucers from users

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './authSlice'
 import invoiceSlice from './invoiceSlice'
+import userSlice from './userSlice'
 
 export const store = configureStore({
-   reducer: { auth: authReducer, invoice: invoiceSlice }
+   reducer: { auth: authReducer, invoice: invoiceSlice, user: userSlice }
 })
