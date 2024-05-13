b9e4085e0efdb3ef5958bc923f32394a4990f426
FerPascualF50 <pascualefernando@gmail.com>
2024-05-13 17:24:51 -0300

    fix: set Loading

--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -12,7 +12,6 @@ import { isEmail, hasPassFormat } from "../utils/validation.js";
 import { validateUserAsync } from '../store/authSlice.js'
 import Loading from '../components/Loading.jsx';
 
-
 const initialState = {
   userName: '',
   password: ''
@@ -53,12 +52,12 @@ const SignIn = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    setLoading(true)
     if (!isEmail(newUser.userName)) return toast.error('ingresa un email válido')
     if (!hasPassFormat(newUser.password)) return toast.error('La contraseña debe tener al menos 8 caracteres y al menos un número')
+    setLoading(true)
     const success = await dispatch(signInAsync(newUser));
-    if (!success.payload.success) return toast.error(`${success.payload.error}.`);
     setLoading(false)
+    if (!success.payload.success) return toast.error(`${success.payload.error}.`);
   }
   const { user } = useSelector((state) => state.auth)
   const isLoggedIn = localStorage.getItem('access_token')
@@ -66,7 +65,7 @@ const SignIn = () => {
   useEffect(() => {
     if (isLoggedIn) {
       setLoading(true)
-      user?.rol === 'admin' ? navigate('/dashboard-admin/users', { replace: true }) : navigate('/dashboard-user', { replace: true })
+      user?.rol === 'admin' ? navigate('/dashboard-admin/users', { replace: true }) : navigate('/dashboard-user/manage', { replace: true })
     }
   }, [isLoggedIn, navigate]);
 
@@ -76,43 +75,17 @@ const SignIn = () => {
       <Toaster position="bottom-center" reverseOrder={false} />
       <CssBaseline />
       <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-          <LockOutlinedIcon />
-        </Avatar>
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}> <LockOutlinedIcon /> </Avatar>
         <Typography component="h1" variant="h5" sx={{ color: 'grey' }}>
           Inicia Sesión
         </Typography>
         <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="e-mail"
-            name="userName"
-            autoComplete="email"
-            autoFocus
-            onChange={handleInput}
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            onChange={handleInput}
-          />
+          <TextField margin="normal" required fullWidth id="email" label="e-mail" name="userName" autoComplete="email" autoFocus onChange={handleInput} />
+          <TextField margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" onChange={handleInput} />
           <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2, color: '#fff' }}>Ingresar</Button>
           <Grid container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '10px' }}>
-            <Grid item xs>
-              <Link href="/forget-pass" variant="body2"> Olvidaste tu clave? </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/signup" variant="body2"> {"No tenes cuenta aún? Registrate aqui"}</Link>
-            </Grid>
+            <Grid item xs> <Link href="/forget-pass" variant="body2"> Olvidaste tu clave? </Link>  </Grid>
+            <Grid item> <Link href="/signup" variant="body2"> {"No tenes cuenta aún? Registrate aqui"}</Link> </Grid>
           </Grid>
         </Box>
       </Box>
