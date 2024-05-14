4b5f495081ed2cee4290772c51a52d54961713d7
FerPascualF50 <pascualefernando@gmail.com>
2024-05-14 01:00:34 -0300

    fix: Links login component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -36,7 +36,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/signup' element={<SignUp />} />
-        <Route path='/login' element={<Login />} />
+        {/* <Route path='/login' element={<Login />} /> */}
         <Route path="/login/:userId" element={<Login />} />
         <Route path="/forget-pass" element={<ForgetPass />} />   
         <Route path="/input-code/:userName" element={<ValidateCodePass />} />
