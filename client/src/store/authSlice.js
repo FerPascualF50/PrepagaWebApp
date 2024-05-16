ef9f0170574c976c6f68d0bf132fe9fad508345b
FerPascualF50 <pascualefernando@gmail.com>
2024-05-15 23:31:55 -0300

    fix: delete console.log

--- client/src/store/authSlice.js
+++ client/src/store/authSlice.js
@@ -138,7 +138,6 @@ const authSlice = createSlice({
         state.user = action.payload
       }),
       builder.addCase(validateUserAsync.fulfilled, (state, action) => {
-        console.log('LA ACTION', action)
         state.user = action.payload
       }),
       builder.addCase(forgetPassAsync.fulfilled, (state, action) => {
